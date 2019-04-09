c623e6895c1226df74ff51064b12c6b4c7025aeb
cx <cxpan1988@gmail.com>
2019-04-09 18:24:24 +0200

    try to add dynamic type

--- src/index_util2.cpp
+++ src/index_util2.cpp
@@ -4,18 +4,29 @@ using namespace seqan;
 
 class DIndex 
 {
+    String <int> dir1
+    String <int> hs1
+    String <uint64_t> dir2;
+    String <uint64_t> hs2;
+    void * pt_dir[2];
+    void * pt_hs[2];
+    int pt;
+    int shape_len;
 public:
-   String <uint64_t> dir;
-   String <uint64_t> sa;
+    DIndex();
+    getDir();
+    getHs();
+    int fullSize();
 }
 unsigned const dshape_len = 14;
 int createDirctIndex(StringSet<String<Dna5> > & seqs, DIndex & index)
 {
     int thd_step;
     LShape shape;
-    resize (index.dir, (4 << dshape_len), 0);
+    resize (index.getDir(), index.fullSize());
     for (int i = 0; i < length(seqs); i++)
     {
+        index.getDirPt()[index.getPt()] ->
         hashInit (shape, begin(seqs[i]));
         for (int j = 0; j < length(seqs); j++)
         {
