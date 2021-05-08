ee772952628c6c52937dda6eaeaf6d19563b9515
sqrt <sqrt1024@gmail.com>
2021-05-08 13:45:26 +0800

    add time meaasurement

--- src/pmpfinder.cpp
+++ src/pmpfinder.cpp
@@ -1584,8 +1584,13 @@ int chainAnchorsBase(String<uint64_t> & anchors, StringSet<String<uint64_t> > & 
     String<ChainsRecord> chain_records;
     std::sort(begin(anchors), end(anchors), _compreAnchor);
     resize (chain_records, length(anchors));
+    double t1 = sysTime();
     getBestChains (anchors, chain_records, it_str, it_end, thd_chain_depth, chn_score.getScore);
+    t1 = sysTime() - t1;
+    double t2 = sysTime();
     traceBackChains(anchors, anchors_chains, chain_records, anchors_chains_score, chn_score.getAbortScore(), bestn);
+    t2 = sysTime() - t2;
+    dout << "cab2" << t1 << t2 << "\n";
     return 0;
 }
 
